a1d19f3e667035577fbfa2c7fb4cbde325dec649
Abdelrahman Hassan Hamdy <abdelrahman_hassan31@yahoo.com>
2025-11-16 13:49:32 +0200

    docs(db): add comments for session configuration options
    * Explain autocommit, autoflush, and session binding
    * Improve code readability and maintainability

--- db.py
+++ db.py
@@ -8,4 +8,9 @@ DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/inventory"
 # connect_args={"sslmode": "require"} to enforce SSL connection
 engine = create_engine(DATABASE_URL)
 
+# autocommit is set to False to manage transactions manually
+# flush is all updates to the database are not committed until explicitly flushed
+# autoflush is set to False to prevent automatic flushing of changes
+# bind=engine to bind the session to the engine that means
+# the session will use this engine to connect to the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
