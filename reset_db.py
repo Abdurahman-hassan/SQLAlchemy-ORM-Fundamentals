7e043fb0e5efe35532fb1b07f3e2da86558ffc21
Abdelrahman Hassan Hamdy <abdelrahman_hassan31@yahoo.com>
2025-11-16 14:15:08 +0200

    docs(db): add docstring for reset_database function
    * Explain the purpose of the function
    * Clarify the use of engine for schema operations

--- reset_db.py
+++ reset_db.py
@@ -3,6 +3,11 @@ from models import Base
 
 
 def reset_database():
+    """
+    Drops all tables and recreates them based on the current models.
+    """
+    # we didn't use session here because we are just dropping and creating tables
+    # this is a schema level operation not a data level operation
+    # because of that we use the engine directly
     Base.metadata.drop_all(bind=engine)
-
     Base.metadata.create_all(bind=engine)
