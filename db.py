e6dfdac28cb8b6e00ec5c388d1729851699e18f6
Abdelrahman Hassan Hamdy <abdelrahman_hassan31@yahoo.com>
2025-11-16 13:12:49 +0200

    docs(db): add comments for SQL logging and connection options
    * Document SQL logging configuration
    * Provide connection options for timezone and SSL

--- db.py
+++ db.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/inventory"
 
+# echo set to True will log all the SQL statements
+# connect_args={"options": "-c timezone=utc"} to set timezone to UTC
+# connect_args={"sslmode": "require"} to enforce SSL connection
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
